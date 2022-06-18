8d7efc4ffeab89c780eb9e6319330245ab7493dd
Chelsea Damora <damora.chelsea@gmail.com>
2022-06-18 00:08:54 -0500

    server is returning reservations data: back-end

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,15 @@
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+
+ const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+ const service = require("./reservations.service")
+
+
+ async function list(req, res, next) {
+  res.json({ data: await service.list() });
 }
 
 module.exports = {
-  list,
+  list: [asyncErrorBoundary(list)],
 };
